5d3b36eaee70c874e3a64ca5724b39294bb34af4
shrekt444 <andrea.maie444@gmail.com>
2021-04-30 17:01:05 +0200

    serie tv e film messi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#root',
     data: {
         films: [],
+        tvSeries: [],
     },
     methods: {
     },
@@ -14,7 +15,28 @@ var app = new Vue({
                 this.films = result.results;
                 console.log(this.films);
 
-                console.log(this.films[0]);
+                console.log('film', this.films[0]);
+
+                // if (lang == 'it' || lang == 'uk') {
+
+                //     nomeImmagine = lang;
+                // }else  {
+                //     nomeImmagine = 'bandieraGenerica';
+                // }
+
+                // var path = './img/' + nomeImmagine + '.jpg';
+
             })
+
+        axios
+            .get('https://api.themoviedb.org/3/search/tv?api_key=4049f8301925a2068e543ebc9f450c23&query=scrubs')
+            .then((response) => {
+                const result = response.data;
+                console.log(result);
+                this.tvSeries = result.results;
+                console.log(this.tvSeries);
+
+                console.log('serie tv', this.tvSeries[0]);
+            })    
     }
 }) 
