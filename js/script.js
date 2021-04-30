1f851418f856bbec993e2c21959e59879382c684
shrekt444 <andrea.maie444@gmail.com>
2021-04-30 10:41:08 +0200

    inizio

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 var app = new Vue({
     el: '#root',
-    data: {    
+    data: {
+        films: [],
     },
     methods: {
     },
     mounted() {
         axios
-            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .get('https://api.themoviedb.org/3/search/movie?api_key=4049f8301925a2068e543ebc9f450c23&query=ritorno al futuro&language=it-IT&page=1')
             .then((response) => {
                 const result = response.data;
                 console.log(result);
-                this.cds = result.response;
-                console.log(this.cds)
-
-                if(ds) {
-
-                }
+                this.films = result.results;
+                console.log(this.films);
             })
     }
 }) 
